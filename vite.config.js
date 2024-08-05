f7a832e681a768218826220f759977b4fd155471
Cristian Villavicencio <cristian_villavicencio@live.com>
2024-08-05 16:30:07 -0400

    ct

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    host: true, // Esto permitirá que el servidor sea accesible en la red local
+    port: 5173 // Puedes cambiar el puerto si lo deseas
+  }
 });
